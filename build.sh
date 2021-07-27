c9b1bcde8548eb00341eeab9d4762513030588ac
m.eremeev <m.eremeev@gmail.com>
2021-07-26 23:20:17 -0700

    Improve build script

--- build.sh
+++ build.sh
@@ -1,19 +1,61 @@
 #!/bin/bash
 
-PREFIX=$( dirname ${BASH_SOURCE[0]} )
+usage() {
+  echo "  Script to build tensor-glue library and tests.
+  usage: build.sh [-h | -c | -t ]
+    options:
+    -h: show usage help
+    -c: clean build - remove all artifacts first
+    -t: run tests after bulding    
+  "
+}
 
-cd  ${PREFIX}
-rm -rf build
-mkdir -p build
-cd build
-cmake ..
+REPO_ROOT=$( dirname ${BASH_SOURCE[0]} )
+CLEAN_BUILD="no"
+RUN_TESTS="no"
+
+while getopts 'hct' OPTION; do
+  case "$OPTION" in
+    h)
+      usage
+      exit 0
+      ;;
+    c)
+      CLEAN_BUILD="yes"
+      ;;
+    t)
+      RUN_TESTS="yes"
+      ;;
+  esac
+done
+
+cd  ${REPO_ROOT}
+
+if [ "$CLEAN_BUILD" == "yes" ]; then
+  	rm -rf build
+	mkdir -p build
+	cd build
+	cmake ..
+else
+	cd build
+	if [ "$RUN_TESTS" == "yes" ]; then
+		rm -f ./tests/platform_info
+		rm -f ./tests/header_only_lib_test
+		rm -f ./tests/binary_lib_test
+	fi
+fi
+
+
 cmake --build .
 
-printf "\n-- Platform info\n"
-./tests/platform_info
 
-printf "\n-- Header-only library test\n"
-./tests/header_only_lib_test
+if [ "$RUN_TESTS" == "yes" ]; then
+	printf "\n-- Platform info\n"
+	./tests/platform_info
 
-printf "\n-- Binary library test\n"
-./tests/binary_lib_test
+	printf "\n-- Header-only library test\n"
+	./tests/header_only_lib_test
+
+	printf "\n-- Binary library test\n"
+	./tests/binary_lib_test
+fi
