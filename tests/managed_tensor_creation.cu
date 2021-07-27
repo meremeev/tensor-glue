2273a2b63fd362aaef892d21ff4ed47e1a37b5a2
m.eremeev <m.eremeev@gmail.com>
2021-07-26 23:18:27 -0700

    new files from  previous commit.

---
+++ tests/managed_tensor_creation.cu
@@ -0,0 +1,159 @@
+#include <catch2/catch_all.hpp>
+#include <cmath>
+#include <iostream>
+
+#include <managed_tensor.cuh>
+
+using namespace tgl;
+
+TEST_CASE( "Create tensor with zero initialization" )
+{
+    ManagedTensor<float> tensor( {2, 3, 3, 4}, true );
+    REQUIRE( tensor.ndims() == 4 );
+    REQUIRE( tensor.size( 1 ) == 3 );
+    REQUIRE( tensor.size( 3 ) == 4 );
+    REQUIRE( tensor.size() == 72 );
+
+    float *p = tensor.data();
+    for ( uint64_t i = 0; i < tensor.size(); ++i, ++p ) {
+        REQUIRE( *p == 0.0 );
+    }
+}
+
+TEST_CASE( "Create int8_t tensor from raw data" )
+{
+    int8_t data[]{1, 2, 3, 4, 5, 6, 7, 8};
+    ManagedTensor<int8_t> tensor( {2, 4}, data );
+
+    auto *p = tensor.data();
+    for ( uint64_t i = 0; i < tensor.size(); ++i, ++p ) {
+        REQUIRE( *p == ( i + 1 ) );
+    }
+}
+
+TEST_CASE( "Create float tensor from raw data" )
+{
+    float data[]{1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0};
+    ManagedTensor<float> tensor( {2, 4}, data );
+
+    float *p = tensor.data();
+    for ( uint64_t i = 0; i < tensor.size(); ++i, ++p ) {
+        REQUIRE( *p == Catch::Approx( i + 1 ) );
+    }
+}
+
+TEST_CASE( "Copy tensor" )
+{
+    ManagedTensor<float> tensor1( {20, 30} );
+    tensor1.fill( 3.14 );
+    ManagedTensor<float> tensor2( tensor1 );
+
+    float *p = tensor2.data();
+    for ( uint64_t i = 0; i < tensor2.size(); ++i, ++p ) {
+        REQUIRE( *p == Catch::Approx( 3.14 ) );
+    }
+}
+
+TEST_CASE( "Move tensor" )
+{
+    ManagedTensor<float> tensor1( {20, 30} );
+    tensor1.fill( 3.14 );
+
+    ManagedTensor<float> tensor2( std::move( tensor1 ) );
+
+    float *p = tensor2.data();
+    for ( uint64_t i = 0; i < tensor2.size(); ++i, ++p ) {
+        REQUIRE( *p == Catch::Approx( 3.14 ) );
+    }
+
+    REQUIRE( tensor1.ndims() == 0 );
+    REQUIRE( tensor1.size() == 0 );
+    REQUIRE( tensor1.data() == nullptr );
+}
+
+TEST_CASE( "Fill tensor with value" )
+{
+    ManagedTensor<float> tensor( {2, 3} );
+    tensor.fill( 3.14 );
+    tensor.sync();
+
+    float *p = tensor.data();
+    for ( int i = 0; i < tensor.size(); ++i, ++p ) {
+        REQUIRE( *p == Catch::Approx( 3.14 ) );
+    }
+}
+
+TEST_CASE( "Fill zeros in tensor" )
+{
+    float data[]{1.2, 0.0, 1.2, 0.0, 1.2, 1.2, 0.0, 1.2};
+    ManagedTensor<float> tensor( {2, 4}, data );
+    tensor.fill_if_zero( 1.2 );
+    tensor.sync();
+
+    float *p = tensor.data();
+    for ( uint64_t i = 0; i < tensor.size(); ++i, ++p ) {
+        REQUIRE( *p == Catch::Approx( 1.2 ) );
+    }
+}
+
+TEST_CASE( "Fill int8_t tensor with random values from uniform distribution" )
+{
+    ManagedTensor<int8_t> tensor( {20, 40} );
+    REQUIRE_NOTHROW( tensor.fill_random_uniform(123) );    
+}
+
+TEST_CASE( "Fill int64_t tensor with random values from uniform distribution" )
+{
+    ManagedTensor<int64_t> tensor( {20, 40} );
+    REQUIRE_NOTHROW( tensor.fill_random_uniform(123) );    
+}
+
+TEST_CASE( "Fill float tensor with random values from uniform distribution" )
+{
+    ManagedTensor<float> tensor( {20, 40} );
+    tensor.fill_random_uniform(123);
+    tensor.sync();
+
+    auto *p = tensor.data();
+    for ( uint64_t i = 0; i < tensor.size(); ++i, ++p ) {
+        auto val = ( 0.0 < *p ) && ( *p <= 1.0 );
+        REQUIRE( val );
+    }
+}
+
+TEST_CASE( "Fill double tensor with random values from uniform distribution" )
+{
+    ManagedTensor<double> tensor( {20, 40} );
+    tensor.fill_random_uniform(123);
+    tensor.sync();
+
+    auto *p = tensor.data();
+    for ( uint64_t i = 0; i < tensor.size(); ++i, ++p ) {
+        auto val = ( 0.0 < *p ) && ( *p <= 1.0 );
+        REQUIRE( val );
+    }
+}
+
+TEST_CASE( "Fill int8_t tensor with random values from normal distribution" )
+{
+    ManagedTensor<int8_t> tensor( {1, 1} );
+    REQUIRE_NOTHROW( tensor.fill_random_normal(123) );   
+}
+
+TEST_CASE( "Fill int64_t tensor with random values from normal distribution" )
+{
+    ManagedTensor<int64_t> tensor( {1, 1} );
+    REQUIRE_NOTHROW( tensor.fill_random_normal(123) );   
+}
+
+TEST_CASE( "Fill float tensor with random values from normal distribution" )
+{
+    ManagedTensor<float> tensor( {2, 4} );
+    REQUIRE_NOTHROW( tensor.fill_random_normal(123) );   
+}
+
+TEST_CASE( "Fill double tensor with random values from normal distribution" )
+{
+    ManagedTensor<double> tensor( {2, 4} );
+    REQUIRE_NOTHROW( tensor.fill_random_normal(123) );   
+}
---
+++ tests/managed_tensor_ops.cu
@@ -0,0 +1,194 @@
+#include <catch2/catch_all.hpp>
+#include <cmath>
+#include <iostream>
+
+#include <managed_tensor.cuh>
+
+using namespace tgl;
+
+TEST_CASE( "Add value to tensor" )
+{
+    ManagedTensor<float> tensor( {2, 3} );
+    tensor.fill( 2.0 ).add( 3L ).add( 1.0F ).add( 4.0L ).sync();
+
+    float* p = tensor.data();
+    for( int i = 0; i < tensor.size(); ++i, ++p ) {
+        REQUIRE( *p == Catch::Approx( 10.0 ) );
+    }
+}
+TEST_CASE( "Subtract value from tensor" )
+{
+    ManagedTensor<float> tensor( {2, 3} );
+    tensor.fill( 12.0 ).sub( 5L ).sub( 0.5F ).sub( 2.0L ).sync();
+
+    float* p = tensor.data();
+    for( int i = 0; i < tensor.size(); ++i, ++p ) {
+        REQUIRE( *p == Catch::Approx( 4.5 ) );
+    }
+}
+
+TEST_CASE( "Multiply tensor by value" )
+{
+    ManagedTensor<float> tensor( {2, 3} );
+    tensor.fill( 2.0 ).mult( 5L ).mult( 0.5F ).mult( 2.0L ).sync();
+
+    float* p = tensor.data();
+    for( int i = 0; i < tensor.size(); ++i, ++p ) {
+        REQUIRE( *p == Catch::Approx( 10.0 ) );
+    }
+}
+
+TEST_CASE( "Divide tensor by value" )
+{
+    ManagedTensor<float> tensor( {2, 3} );
+    tensor.fill( 21.0 ).div( 3L ).div( 0.5F ).div( 2.0L ).sync();
+
+    float* p = tensor.data();
+    for( int i = 0; i < tensor.size(); ++i, ++p ) {
+        REQUIRE( *p == Catch::Approx( 7.0 ) );
+    }
+}
+
+TEST_CASE( "Tensor fmod()" )
+{
+    ManagedTensor<float> tensor( {2, 3} );
+    tensor.fill( 21.7 ).fmod( 15L ).fmod( 4.5F ).fmod( 2L ).sync();
+
+    float* p = tensor.data();
+    for( int i = 0; i < tensor.size(); ++i, ++p ) {
+        REQUIRE( *p == Catch::Approx( 0.2 ) );
+    }
+}
+
+TEST_CASE( "Add tensors (same type)" )
+{
+    ManagedTensor<float> tensor1( {200, 300} );
+    tensor1.fill( 2.0 );
+    ManagedTensor<float> tensor2( {200, 300} );
+    tensor2.fill( 8.0 ).add( tensor1 ).sync();
+
+    float* p = tensor2.data();
+    for( int i = 0; i < tensor2.size(); ++i, ++p ) {
+        REQUIRE( *p == Catch::Approx( 10.0 ) );
+    }
+}
+
+TEST_CASE( "Add tensors (different types)" )
+{
+    ManagedTensor<float> tensor1( {20, 30} );
+    tensor1.fill( 20.0 );
+    ManagedTensor<double> tensor2( {20, 30} );
+    tensor2.fill( -8.0 );
+    ManagedTensor<std::int64_t> tensor3( {20, 30} );
+    tensor3.fill( 3 );
+    ManagedTensor<std::int8_t> tensor4( {20, 30} );
+    tensor4.fill( -5 );
+    tensor1.add( tensor2 ).add( tensor3 ).add( tensor4 ).sync();
+
+    float* p = tensor1.data();
+    for( int i = 0; i < tensor1.size(); ++i, ++p ) {
+        REQUIRE( *p == Catch::Approx( 10.0 ) );
+    }
+}
+
+TEST_CASE( "Multiply tensors (same type)" )
+{
+    ManagedTensor<float> tensor1( {20, 30} );
+    tensor1.fill( 2.0 );
+    ManagedTensor<float> tensor2( {20, 30} );
+    tensor2.fill( 5.0 ).mult( tensor1 ).sync();
+
+    float* p = tensor2.data();
+    for( int i = 0; i < tensor2.size(); ++i, ++p ) {
+        REQUIRE( *p == Catch::Approx( 10.0 ) );
+    }
+}
+
+TEST_CASE( "Tensor neg()" )
+{
+    ManagedTensor<float> tensor( {2, 3} );
+    tensor.fill( 2.0 ).neg().sync();
+
+    float* p = tensor.data();
+    for( int i = 0; i < tensor.size(); ++i, ++p ) {
+        REQUIRE( *p == Catch::Approx( -2.0 ) );
+    }
+}
+
+TEST_CASE( "Tensor recip()" )
+{
+    ManagedTensor<float> tensor( {2, 3} );
+    tensor.fill( 2.0 ).recip().add( 2 ).sync();
+
+    float* p = tensor.data();
+    for( int i = 0; i < tensor.size(); ++i, ++p ) {
+        REQUIRE( *p == Catch::Approx( 2.5 ) );
+    }
+}
+
+TEST_CASE( "Tensor  exp()" )
+{
+    ManagedTensor<double> tensor( {2, 3} );
+    tensor.fill( 2.0 ).exp().sync();
+
+    double* p = tensor.data();
+    for( int i = 0; i < tensor.size(); ++i, ++p ) {
+        REQUIRE( *p == Catch::Approx( exp( 2.0 ) ) );
+    }
+}
+
+TEST_CASE( "Tensor fabs()" )
+{
+    float data[]{1, -2, -3.4, -4.6, 5.8, -6., -7.2, -8};
+    ManagedTensor<float> tensor1( {2, 4}, data );
+    ManagedTensor<float> tensor2( tensor1 );
+    tensor2.fabs().sync();
+
+    float* p1 = tensor1.data();
+    float* p2 = tensor2.data();
+    for( uint64_t i = 0; i < tensor1.size(); ++i, ++p1, ++p2 ) {
+        REQUIRE( *p2 == Catch::Approx( fabs( *p1 ) ) );
+    }
+}
+
+TEST_CASE( "Tensor log()" )
+{
+    float data[]{0.1, 0.2, 3.4, 4.6, 5.8, 6., 7.2, 8};
+    ManagedTensor<float> tensor1( {2, 4}, data );
+    ManagedTensor<float> tensor2( tensor1 );
+    tensor2.log().sync();
+
+    float* p1 = tensor1.data();
+    float* p2 = tensor2.data();
+    for( uint64_t i = 0; i < tensor1.size(); ++i, ++p1, ++p2 ) {
+        REQUIRE( *p2 == Catch::Approx( log( *p1 ) ) );
+    }
+}
+
+TEST_CASE( "Tensor log10()" )
+{
+    float data[]{0.1, 0.2, 3.4, 4.6, 5.8, 6., 7.2, 8};
+    ManagedTensor<float> tensor1( {2, 4}, data );
+    ManagedTensor<float> tensor2( tensor1 );
+    tensor2.log10().sync();
+
+    float* p1 = tensor1.data();
+    float* p2 = tensor2.data();
+    for( uint64_t i = 0; i < tensor1.size(); ++i, ++p1, ++p2 ) {
+        REQUIRE( *p2 == Catch::Approx( log10( *p1 ) ) );
+    }
+}
+
+TEST_CASE( "Tensor sqrt()" )
+{
+    float data[]{0.1, 0.2, 3.4, 4.6, 5.8, 6., 7.2, 8};
+    ManagedTensor<float> tensor1( {2, 4}, data );
+    ManagedTensor<float> tensor2( tensor1 );
+    tensor2.sqrt().sync();
+
+    float* p1 = tensor1.data();
+    float* p2 = tensor2.data();
+    for( uint64_t i = 0; i < tensor1.size(); ++i, ++p1, ++p2 ) {
+        REQUIRE( *p2 == Catch::Approx( sqrt( *p1 ) ) );
+    }
+}
